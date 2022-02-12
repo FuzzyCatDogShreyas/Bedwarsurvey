d084b6888f5fc13d19f41a1eb1c0f4d737f0d57f
FuzzyCatDogShreyas <94261316+FuzzyCatDogShreyas@users.noreply.github.com>
2022-02-11 19:45:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var game=prompt("How many Minecraft Bedwars games did you win?")
+if(game>5)
+{
+    window.alert("Nice! You're pretty good haha")
+}
+else
+{
+    window.alert("You suck! Uninstall Minecraft loser!")
+}
